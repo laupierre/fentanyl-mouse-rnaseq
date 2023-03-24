d894b42ca4a9e64f063bc9781d3b90477bc7008b
laupierre <laupierre@users.noreply.github.com>
2023-03-24 10:44:38 +0100

    Create deseq2_two_groups.R

---
+++ deseq2_two_groups.R
@@ -0,0 +1,22 @@
+library (openxlsx)
+library (DESeq2)
+library (ggplot2)
+
+system ("mkdir ./output")
+
+anno <- read.delim ("gencode.vM32.annotation.txt")
+anno <- anno[ ,grep ("transcript_id", colnames (anno), invert=TRUE)]
+anno <- unique (anno)
+
+a <- read.delim ("./projects/star_results/subread.counts.txt", skip=1)
+a <- a[ ,grep ("Gene|bam", colnames (a))]
+
+a <- merge (a, anno, by.x="Geneid", by.y="gene_id", all.x=TRUE) 
+
+a <- a[grep ("miRNA|Mt_tRNA|Mt_rRNA|rRNA|snRNA|snoRNA|scRNA|sRNA|misc_RNA|scaRNA|ribozyme|IG_|TR_", a$gene_type, invert=TRUE), ]
+colnames (a) <- gsub ("Aligned.out.bam", "", colnames (a))
+colnames (a) <- gsub ("star.", "", colnames (a))
+
+  
+write.xlsx (a, "./output/star_gene_raw_counts.xlsx", rowNames=F)
+
