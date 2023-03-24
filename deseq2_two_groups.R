a92f7941bc1b5ee3ed489cba1637b82b83a450c6
laupierre <laupierre@users.noreply.github.com>
2023-03-24 11:56:44 +0100

    Update deseq2_two_groups.R

--- deseq2_two_groups.R
+++ deseq2_two_groups.R
@@ -108,7 +108,7 @@ ggplot(pcaData, aes(PC1, PC2, color=condition, shape=sex)) +
   		geom_point(size=3) +
   		xlab(paste0("PC1: ",percentVar[1],"% variance")) +
   		ylab(paste0("PC2: ",percentVar[2],"% variance")) + 
-		  coord_fixed ()
+		coord_fixed ()
 
 
 
